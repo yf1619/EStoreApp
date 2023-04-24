ca05028d01f4f0876ed35b4cc7226ad56cce390c
yiwei <yf1619@ic.ac.uk>
2023-04-24 18:32:11 +0800

    First Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+const express = require("express");
+
+const errorController = require("./controllers/error");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.resolve("public")));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+app.listen(3000);
