7cdb24cd2ca12ce46ddc449448bae304541c32e7
yiwei <yf1619@ic.ac.uk>
2023-08-29 17:50:07 +0800

    Section 10
    Learn how to use mysql2 to connect nodejs, and use fixed query language to access db

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require("path");
 const express = require("express");
 
 const errorController = require("./controllers/error");
+const db = require("./util/database");
 
 const app = express();
 
